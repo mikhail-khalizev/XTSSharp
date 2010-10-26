f91d30611df6dd9444d46c8d274e83360272636b
garethl <garethl@gmail.com>
2010-10-26 23:10:22 +0200

    * Renaming projects & names * Added BSD license to all files, and to a license.txt file.

---
+++ src/XTSSharp.Test/RandomInfiniteStream.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) 2010 Gareth Lennox (garethl@dwakn.com)
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of Gareth Lennox nor the names of its
+//       contributors may be used to endorse or promote products derived from this
+//       software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.IO;
+
+namespace XTSSharp.Test
+{
+	class RandomInfiniteStream : Stream
+	{
+		private readonly Random _random;
+
+		public RandomInfiniteStream()
+		{
+			_random = new Random();
+		}
+
+		public RandomInfiniteStream(int seed)
+		{
+			_random = new Random(seed);
+		}
+
+		public override bool CanRead
+		{
+			get { return true; }
+		}
+
+		public override bool CanSeek
+		{
+			get { return false; }
+		}
+
+		public override bool CanWrite
+		{
+			get { return false; }
+		}
+
+		public override long Length
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override long Position
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		public override void Flush()
+		{
+		}
+
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void SetLength(long value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			if (offset != 0 && count != buffer.Length)
+			{
+				//bleh
+				var bleh = new byte[count];
+				_random.NextBytes(bleh);
+
+				Buffer.BlockCopy(bleh, 0, buffer, offset, count);
+			}
+			else
+			{
+				_random.NextBytes(buffer);
+			}
+
+			return count;
+		}
+
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
