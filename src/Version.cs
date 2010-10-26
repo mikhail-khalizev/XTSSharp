40dd1e5b2f8cc395f083e084cc60a091261cd8cc
garethl <garethl@gmail.com>
2010-10-26 23:15:38 +0200

    * Central version file.

---
+++ src/Version.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Gareth Lennox")]
+[assembly: AssemblyProduct("XTSSharp")]
+[assembly: AssemblyCopyright("Copyright © 2010 Gareth Lennox")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
